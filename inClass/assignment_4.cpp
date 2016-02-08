e51bc7c430ee16e4e0546537be8b9e16b5f2a184
Bernin A. Uben <BerninUben@icloud.com>
2016-02-08 10:52:14 -0500

    fixed ReverseDisplay() fucntionality && syntax Err in main() switch statement

--- inClass/assignment_4.cpp
+++ inClass/assignment_4.cpp
@@ -58,7 +58,7 @@ public:
 
 int main() {
     DLinkList List; // Initialize a list called 'List'
-    cout << "Please enter information on five unique individuals:\n\n";
+    cout << "Enter information on five unique individuals:\n\n";
     for (int x = 0; x < 2; x++) {
         cout << x+1 << ". ";
         List.Insert();
@@ -78,6 +78,7 @@ int main() {
                 break;
             case 3:
                 List.ReverseDisplay();
+                break;
             default:
                 cout << "INVALID OPTION\n\n";
                 break;
@@ -100,7 +101,7 @@ int main() {
 
 void DLinkList::Insert() {  // Append a node to the linked list
     Node*  newNode = new Node(); // Create a new node
-    Node*  prevNode;
+    Node*  prevNode = NULL;
     string fName; // node content
     string lName;
     string address;
@@ -112,6 +113,7 @@ void DLinkList::Insert() {  // Append a node to the linked list
     
     // use the derefrencer, '->', instead of the dot operator to use class methods/functions
     newNode->SetNode(fName, lName, address, phoneNum); // Set the default values as the nodes values
+    newNode->SetPrevNode(NULL); // Set the nodes prev value as 'NULL', and will stay like that if at the start of a doubly linked list to mark an end
     newNode->SetNextNode(NULL); // Set the nodes next value as 'NULL', the end of the list
     
     Node *temp = head; // Create a temp pointer, Set the 'temp' pointer variable of type Node equal to the head of the list... ->
@@ -124,21 +126,30 @@ void DLinkList::Insert() {  // Append a node to the linked list
         }
         temp->SetNextNode(newNode); // As soon as the while loop ends b/c the current 'next' pointer is pointing to NULL/the end of list... ->
         // Set the current nodes 'next' pointer to the recently made node
-        temp->SetPrevNode(prevNode);
-        if ( temp->getNext() == NULL ) { tail = newNode; } // Sets the tail of the doubly linked list so it can look through the list in reverse
-    } else {
-        newNode->SetPrevNode(NULL);
+        temp->SetPrevNode(prevNode); // sets the node address in mem before the current node as the pointer 'prev' in the current node
+        
+        // under here we have to set the newly added node as the tail and set the current node as its previious node so it can go back to the beginning.
+        while ( temp->getNext() != NULL ){
+            prevNode = temp; // set the current node as the node before the next node AGAIN
+            temp = temp->getNext(); // new node was set after the current node so we go through to the end of the list AGAIN
+        }
+        if ( temp->getNext() == NULL ){
+            temp->SetPrevNode(prevNode); // sets the node address in mem before the current node as the pointer 'prev' in the current node
+            tail = temp; // Sets the tail of the doubly linked list to make it possible to look through the list in reverse // could also be '{ tail = newNode}'
+        }
+    } else { // this executes if the list is empty
+        tail = newNode; // after the node has been added to the empty array, its not empty anymore, we set it as the tail becuase thier is nothing behind it so its 'prev' variable is set to NULL
         head = newNode; // 'if (temp == NULL)' if the head is pointing to the end of the list, there are no nodes in the list so... ->
-    }                 // Set the node, that was just made, as the head of the list if NULL is the first thing after the head.
+    }                   // Set the node, that was just made, as the head of the list if NULL is the first thing after the head.
 }
 
 void DLinkList::Search(){
-    string last;
+    string last; // Intake the last name the user will want the program to search for in the doublely linked list
     cout << "Enter a last name: ";
     cin >> last;
     
     Node *temp = head; // Create a temp pointer, Set the 'temp' pointer variable of type Node equal to the head of the list... ->
-    // so that 'temp' is used as an alias for 'head' in
+                       // so that 'temp' is used as an alias for 'head' in
     if (head != NULL) {
         if (temp->getLName() != last) {
             while (temp->getLName() != last) {
@@ -171,17 +182,20 @@ void DLinkList::Display() { // Print the contents of the list
 }
 
 void DLinkList::ReverseDisplay() {
-    Node *temp = tail; // Create a temp pointer, Set the 'temp' pointer variable of type Node equal to the head of the list... ->
-    // so that 'temp' is used as an alias for 'head' in
+    Node *temp = tail; // Create a temp pointer, Set the 'temp' pointer variable of type Node equal to the tail of the list... ->
+    // so that 'temp' is used as an alias for 'tails'
     
     cout << "\n\nList Contents: \n";
     if ( temp == NULL) {
         cout << "There's nothing in the list D:\n\n";
-    } else if ( temp->getNext() == NULL ) {  // If theres only one node in the list print this out
+    } else if ( temp->getPrev() == NULL ) {  // If theres only one node in the list print this out and because were already at the tail, No need to check if temp is == NULL because the first 'if' already did
         cout << temp->getFName() << " " << temp->getLName() << ",\n" << temp->getAddress() << ",\n" << temp->getNum() << "  --->  NULL\n\n";
     } else {
-        while ( temp != NULL ) {  // If there are multiple nodes in the list, then we print out its information
+        while ( temp->getPrev() != NULL || head == temp ) {  // while there is a node before the current node in the list OR the current node is the head THEN continue the loop
             cout << temp->getFName() << " " << temp->getLName() << ",\n" << temp->getAddress() << ",\n" << temp->getNum() << "  --->\n\n"; // print current node
+            if (head == temp) {
+                break;
+            }
             temp = temp->getPrev(); // get the next node
         }
         cout << "NULL\n\n" << endl;
