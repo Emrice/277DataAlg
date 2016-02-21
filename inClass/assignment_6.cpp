c6d5b4f686c67c127bc06e56447c99aacb1c83de
Bernin A. Uben <BerninUben@icloud.com>
2016-02-21 17:22:02 -0500

    In-Class Assignment: 'STL Vector'

--- inClass/assignment_6.cpp
+++ inClass/assignment_6.cpp
@@ -1,6 +1,6 @@
 //
-//  Homework_6.cpp
-//  homework #6
+//  Assignment_6.cpp
+//  Assignment #6
 //
 //  Created by Bernin A. Uben on 2/20/16.
 //  Copyright © 2016 Bernin A. Uben. All rights reserved.
@@ -12,14 +12,14 @@
 //
 
 #include <iostream>
-#include <vector>
-using std::vector;
-using namespace std;
+#include <vector>     // Needed for Vector to work
+using std::vector;    // Needed for Vector to work
+using namespace std;  
 
 void display(const vector<int>& data){
   cout << "Vector Contents: (";
-  for (unsigned int i = 0; i < data.size(); i++) {
-    if(i == data.size()-1){
+  for (unsigned int i = 0; i < data.size(); i++) { // Unsigned int is so that no negative numbers are possible
+    if(i == data.size()-1){                        // If the current item in the vector is the biggest position in the vector
       cout << data[i];
     } else {
       cout << data[i] << ",";
@@ -29,59 +29,59 @@ void display(const vector<int>& data){
 }
 
 void val_in_pos(int pos, const vector<int>& data){
-  try{
+  try{                                                    // Start of exception handling
     
-    if(data.size() < pos+1){
-      throw "position specified is too big!   |   ";
+    if(data.size() < pos+1){                              // If the size of the vector is less than the position the user wants
+      throw "position specified is too big!   |   ";      // Throw the exception
     } else {
       cout << "\nValue in Position " << pos << ": " << data.at(pos) << "   |   "; // Could also do 'data[pos]'
     }
     
-  } catch(const char *err){ cout << "\nERROR: " << err; }
+  } catch(const char *err){ cout << "\nERROR: " << err; } // Part of the exception
   
-  display(data);
+  display(data);                                          // Show the Vector after showing the POS
 }
 
 int main() {
-  vector<int> data;
+  vector<int> data;                                       // Initialize an integer vector called data
   
   cout << "Inserting 7 in position 1:\n";
-  data.insert(data.begin() + 0, 7);
-  display(data);
+  data.insert(data.begin() + 0, 7);                       // Insert the number 7 in the 1st position in the vector
+  display(data);                                          // Shows the contents of the vector its called on
   
   cout << "\nInserting 4 in position 0:\n";
-  data.insert(data.begin() + 0, 4);
-  display(data);
+  data.insert(data.begin() + 0, 4);                       // Insert the number 4, 0 positions away from the index of the vector
+  display(data);                                          // Shows the contents of the vector its called on
   
-  val_in_pos(1, data);
+  val_in_pos(1, data);                                    // Displays the value in position 1 and then displays the contents of the vector
   
   cout << "\nInserting 2 in position 2:\n";
-  data.insert(data.begin() + 2, 2);
-  display(data);
+  data.insert(data.begin() + 2, 2);                       // Insert the number 2, 2 positions away from the index of the vector
+  display(data);                                          // Shows the contents of the vector its called on
   
-  val_in_pos(3, data);  // Supposed to put out 'error'
+  val_in_pos(3, data);                                    // Supposed to put out 'error' through exception handling
   
   cout << "\nErasing value in position 1:\n";
-  data.erase(data.begin() + 1);
-  display(data);
+  data.erase(data.begin() + 1);                           // Erase the number in the position 1
+  display(data);                                          // Shows the contents of the vector its called on
   
   cout << "\nInserting 5 in position 1:\n";
-  data.insert(data.begin() + 1, 5);
-  display(data);
+  data.insert(data.begin() + 1, 5);                       // Insert the number 5, 1 positions away from the index of the vector
+  display(data);                                          // Shows the contents of the vector its called on
   
   cout << "\nInserting 3 in position 1:\n";
-  data.insert(data.begin() + 1, 3);
-  display(data);
+  data.insert(data.begin() + 1, 3);                       // Insert the number 3, 1 positions away from the index of the vector
+  display(data);                                          // Shows the contents of the vector its called on
   
   cout << "\nInserting 9 in position 4:\n";
-  data.insert(data.begin() + 4, 9);
-  display(data);
+  data.insert(data.begin() + 4, 9);                       // Insert the number 9, 4 positions away from the index of the vector
+  display(data);                                          // Shows the contents of the vector its called on
   
-  val_in_pos(2, data);
+  val_in_pos(2, data);                                    // Displays the value in position 2 and then displays the contents of the vector
   
   cout << "\nSetting value in position 3 to 8:\n";
-  data.at(3) = 8;
-  display(data);
+  data.at(3) = 8;                                         // Set the number in position 3 to 8 (2 -> 8)
+  display(data);                                          // Shows the contents of the vector its called on
   
   cout << "\n";
   return EXIT_SUCCESS;
